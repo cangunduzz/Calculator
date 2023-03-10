846e9475e1261be1267119fc9d1a90d1277705cc
Coders Club <hi@codersclub.com>
2023-03-10 16:47:29 +0300

    Added Alert Box

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function val(result) {
   form.disp.value = form.disp.value + result;
 }
+
 function calculate() {
+  var myAlert = document.querySelector(".alert");
   if (form.disp.value == "") {
-    alert("Lütfen Rakam Giriniz");
+    myAlert.classList.remove("hide");
+    myAlert.classList.add("show");
+    myAlert.classList.add("showAlert");
+    setTimeout(function () {
+      myAlert.classList.add("hide");
+      myAlert.classList.remove("show");
+    }, 5000);
   } else {
     form.disp.value = eval(form.disp.value);
   }
@@ -11,4 +19,10 @@ function calculate() {
 
 function cleantable() {
   form.disp.value = ""; //When we click on equal button the result will be clear
+}
+
+function closeAlert() {
+  var myAlert = document.querySelector(".alert");
+  myAlert.classList.add("hide");
+  myAlert.classList.remove("show");
 }
